a7d2f40549b1e52f86581043d2ac8364673559ba
Callum Simpson <callumjohnsimpson@hotmail.com>
2021-02-17 16:42:04 +0000

    start of slide system for hero

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
+"use strict";
+
 menuToggler.addEventListener('click', ev =>{
   menuToggler.classList.toggle('open');
 
 });
+
+const slides = document.querySelectorAll('#slides section');
+
+function setSlide(slide_number) {
+  let displayed = document.querySelector('#slides section.displayed');
+  if(displayed) {
+      displayed.classList.remove('displayed');
+  }
+  let visible_slide = slides.item(slide_number);
+  if(visible_slide){
+    visible_slide.classList.add('displayed');
+  }
+}
+
+setSlide(0);
