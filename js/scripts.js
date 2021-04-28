37871d577da7d90c31a4333c66012a5cf4319923
CallumSi <p2444354@my365.dmu.ac.uk>
2021-04-28 22:00:18 +0100

    Updated code readabilty again to encoporate given feedback.

--- js/scripts.js
+++ js/scripts.js
@@ -2,42 +2,55 @@
 
 //toggle navbar
 
-menuToggler.addEventListener('click', ev =>{
-  slideManager.classList.toggle('hidden')
-  menuToggler.classList.toggle('open');
+menuToggler.addEventListener('click', ev => {
+    slideManager.classList.toggle('hidden')
+    menuToggler.classList.toggle('open');
 });
 
 //code to close the nav when navigating to the same page
 
-itemstoreButton.addEventListener('click', ev =>{
-  menuToggler.classList.remove('open');
+itemstoreButton.addEventListener('click', ev => {
+    menuToggler.classList.remove('open');
 });
 
 //change the what the nav looks like on scroll
 
-window.onscroll = function () {
-  const header = document.querySelector('header');
-  const navLinks = document.querySelectorAll('header nav a');
-  const menuTogglerdivs = document.querySelectorAll('#menuToggler div');
-  if (window.scrollY > 100 ){
-    for(const menuTogglerdiv of menuTogglerdivs){menuTogglerdiv.classList.add("menuBlack")}
-    for(const navLink of navLinks){navLink.classList.add("navLinkBlack")}
-    header.classList.add("headerWhite")
-  }else{
-    for(const menuTogglerdiv of menuTogglerdivs){menuTogglerdiv.classList.remove("menuBlack")}
-    for(const navLink of navLinks){navLink.classList.remove("navLinkBlack")}
-    header.classList.remove("headerWhite")
-  }
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const navLinks = document.querySelectorAll('header nav a');
+    const menuTogglerdivs = document.querySelectorAll('#menuToggler div');
+    if (window.scrollY > 100) {
+        for (const menuTogglerdiv of menuTogglerdivs) {
+            menuTogglerdiv.classList.add("menuBlack")
+        }
+        for (const navLink of navLinks) {
+            navLink.classList.add("navLinkBlack")
+        }
+        header.classList.add("headerWhite")
+    } else {
+        for (const menuTogglerdiv of menuTogglerdivs) {
+            menuTogglerdiv.classList.remove("menuBlack")
+        }
+        for (const navLink of navLinks) {
+            navLink.classList.remove("navLinkBlack")
+        }
+        header.classList.remove("headerWhite")
+    }
 };
 
 // set the slideshow to a given slide
 
 const slides = document.querySelectorAll('#slides section');
+
 function setSlide(slide_number) {
-  let displayed = document.querySelector('#slides section.displayed');
-  if(displayed) {displayed.classList.remove('displayed');}
-  let visible_slide = slides.item(slide_number);
-  if(visible_slide){visible_slide.classList.add('displayed');}
+    let displayed = document.querySelector('#slides section.displayed');
+    if (displayed) {
+        displayed.classList.remove('displayed');
+    }
+    let visible_slide = slides.item(slide_number);
+    if (visible_slide) {
+        visible_slide.classList.add('displayed');
+    }
 }
 
 //slide button functionality
@@ -45,41 +58,64 @@ function setSlide(slide_number) {
 let selected = 0;
 const elements = document.querySelectorAll('#slideManager div');
 const toggleWhite = (ev) => {
-  if (ev.target.id==("slidebutton1")){selected=0;}
-  if (ev.target.id==("slidebutton2")){selected=1;}
-  if (ev.target.id==("slidebutton3")){selected=2;}
-  for(const element of elements){element.classList.remove("buttonWhite")}
-  ev.target.classList.add("buttonWhite")
-  setSlide(selected)
+    if (ev.target.id == ("slidebutton1")) {
+        selected = 0;
+    }
+    if (ev.target.id == ("slidebutton2")) {
+        selected = 1;
+    }
+    if (ev.target.id == ("slidebutton3")) {
+        selected = 2;
+    }
+    for (const element of elements) {
+        element.classList.remove("buttonWhite")
+    }
+    ev.target.classList.add("buttonWhite")
+    setSlide(selected)
 }
-for (const element of elements) {element.addEventListener('click', toggleWhite)
+for (const element of elements) {
+    element.addEventListener('click', toggleWhite)
 }
 
 //go between slides with interval
 
-window.setInterval(function () {
-  selected++;
-  for(const element of elements){element.classList.remove("buttonWhite")}
-  if (selected==3){selected=0;}
-  if (selected==0){slidebutton1.classList.add("buttonWhite")}
-  if (selected==1){slidebutton2.classList.add("buttonWhite")}
-  if (selected==2){slidebutton3.classList.add("buttonWhite")}
-  setSlide(selected);
+window.setInterval(function() {
+    selected++;
+    for (const element of elements) {
+        element.classList.remove("buttonWhite")
+    }
+    if (selected == 3) {
+        selected = 0;
+    }
+    if (selected == 0) {
+        slidebutton1.classList.add("buttonWhite")
+    }
+    if (selected == 1) {
+        slidebutton2.classList.add("buttonWhite")
+    }
+    if (selected == 2) {
+        slidebutton3.classList.add("buttonWhite")
+    }
+    setSlide(selected);
 }, 5000);
 
 // search by item name
 
-storeSearch.addEventListener('input', ev =>{
-  const sections = Array.from(document.querySelectorAll('#itemstore article')).filter(section =>{
-    return !section.dataset.name.includes(storeSearch.value.toLowerCase());
-  });
-  for(const result of document.querySelectorAll('.hidden')){result.classList.remove('hidden');}
-  for(const section of sections){
-    if (section.classList.contains('searchbar')){
-      //pass
-    }else{section.classList.add('hidden');}
-  }
-  loaderText.classList.add("hidden");
+storeSearch.addEventListener('input', ev => {
+    const sections = Array.from(document.querySelectorAll('#itemstore article')).filter(section => {
+        return !section.dataset.name.includes(storeSearch.value.toLowerCase());
+    });
+    for (const result of document.querySelectorAll('.hidden')) {
+        result.classList.remove('hidden');
+    }
+    for (const section of sections) {
+        if (section.classList.contains('searchbar')) {
+            //pass
+        } else {
+            section.classList.add('hidden');
+        }
+    }
+    loaderText.classList.add("hidden");
 });
 
 
@@ -88,69 +124,69 @@ storeSearch.addEventListener('input', ev =>{
 
 //loads differnt api URL's
 async function loadObject(url) {
-  const response = await fetch(url);
-  return response.json();
+    const response = await fetch(url);
+    return response.json();
 }
 
 
 //Convert object data into DOM elements
 function buildArticleFromData(specificitem, id, price) {
-  //deconstruct the array
-  const temp=((Object.entries(specificitem)[0][1]))
-  //create the  elements
-  const article = document.createElement("article");
-  const h3 = document.createElement("h3");
-  const h4 = document.createElement("h4");
-  const img = document.createElement("img");
-  const storelink = document.createElement("a")
-  const workshoplink = document.createElement("a")
-  //acessing the data
-  const datatag = (Object.entries(temp)[0][1].tags[1]);
-  const specificworkshoplink=(Object.entries(temp)[0][1].actions[0].link);
-  const dataname = (Object.entries(temp)[0][1].name);
-  h3.innerText=(Object.entries(temp)[0][1].name);
-  h4.innerText=(Object.entries(temp)[0][1].tags[0].name);
-  img.src=("https://steamcommunity-a.akamaihd.net/economy/image/"  + Object.entries(temp)[0][1].icon_url_large);
-  storelink.text = "£" + (price/100).toFixed(2);
-  storelink.href = "https://store.steampowered.com/itemstore/252490/detail/" + id + "/"
-  storelink.target = "_blank";
-  workshoplink.text = "View";
-  workshoplink.href = specificworkshoplink;
-  workshoplink.target = "_blank";
-  //add each item
-  article.appendChild(img);
-  article.appendChild(h3);
-  article.appendChild(h4);
-  article.appendChild(storelink);
-  article.appendChild(workshoplink);
-  article.classList.add("storeitem");
-  article.setAttribute("data-name", dataname.toLowerCase());
-  return article;
+    //deconstruct the array
+    const temp = ((Object.entries(specificitem)[0][1]))
+    //create the  elements
+    const article = document.createElement("article");
+    const h3 = document.createElement("h3");
+    const h4 = document.createElement("h4");
+    const img = document.createElement("img");
+    const storelink = document.createElement("a")
+    const workshoplink = document.createElement("a")
+    //acessing the data
+    const datatag = (Object.entries(temp)[0][1].tags[1]);
+    const specificworkshoplink = (Object.entries(temp)[0][1].actions[0].link);
+    const dataname = (Object.entries(temp)[0][1].name);
+    h3.innerText = (Object.entries(temp)[0][1].name);
+    h4.innerText = (Object.entries(temp)[0][1].tags[0].name);
+    img.src = ("https://steamcommunity-a.akamaihd.net/economy/image/" + Object.entries(temp)[0][1].icon_url_large);
+    storelink.text = "£" + (price / 100).toFixed(2);
+    storelink.href = "https://store.steampowered.com/itemstore/252490/detail/" + id + "/"
+    storelink.target = "_blank";
+    workshoplink.text = "View";
+    workshoplink.href = specificworkshoplink;
+    workshoplink.target = "_blank";
+    //add each item
+    article.appendChild(img);
+    article.appendChild(h3);
+    article.appendChild(h4);
+    article.appendChild(storelink);
+    article.appendChild(workshoplink);
+    article.classList.add("storeitem");
+    article.setAttribute("data-name", dataname.toLowerCase());
+    return article;
 }
 
 
 //function to insert articles into the page
 async function insertArticles() {
-  loader.classList.add("waiting");
-  //get list of items in store
-  const obj = await loadObject("https://corsanywhere.herokuapp.com/https://api.steampowered.com/ISteamEconomy/GetAssetPrices/v1/?key=DF7C9821FA297EB257123E68B0E9E1DD&appid=252490");
-  //loop through each item
-  const itemStoreObjects = obj.result.assets;
-  for(const storeitem of itemStoreObjects){
-      //search the specific item via its id
-      const specificitem = await loadObject(`https://corsanywhere.herokuapp.com/https://api.steampowered.com/ISteamEconomy/GetAssetClassInfo/v1/?key=DF7C9821FA297EB257123E68B0E9E1DD&appid=252490&class_count=1&classid0=${storeitem.classid}`);
-      const article = buildArticleFromData(specificitem, storeitem.name, storeitem.original_prices.GBP);
-      itemstore.appendChild(article);
+    loader.classList.add("waiting");
+    //get list of items in store
+    const obj = await loadObject("https://corsanywhere.herokuapp.com/https://api.steampowered.com/ISteamEconomy/GetAssetPrices/v1/?key=DF7C9821FA297EB257123E68B0E9E1DD&appid=252490");
+    //loop through each item
+    const itemStoreObjects = obj.result.assets;
+    for (const storeitem of itemStoreObjects) {
+        //search the specific item via its id
+        const specificitem = await loadObject(`https://corsanywhere.herokuapp.com/https://api.steampowered.com/ISteamEconomy/GetAssetClassInfo/v1/?key=DF7C9821FA297EB257123E68B0E9E1DD&appid=252490&class_count=1&classid0=${storeitem.classid}`);
+        const article = buildArticleFromData(specificitem, storeitem.name, storeitem.original_prices.GBP);
+        itemstore.appendChild(article);
     }
-  }
+}
 
-  //loads the page
-  async function loadPage() {
+//loads the page
+async function loadPage() {
     loader.classList.add("waiting");
     await insertArticles();
     loader.classList.remove("waiting");
     loaderText.classList.add("hidden");
-  }
+}
 
 //load the page on startup
-  loadPage()
+loadPage()
--- js/yourItems.js
+++ js/yourItems.js
@@ -3,179 +3,198 @@
 //declare some variables
 
 let pageSize = 12;
-let currentPage =1;
+let currentPage = 1;
 let yourItemsArray;
 let searchcriteria = "";
 
 //toggle navbar
 
-menuToggler.addEventListener('click', ev =>{
-  menuToggler.classList.toggle('open');
+menuToggler.addEventListener('click', ev => {
+    menuToggler.classList.toggle('open');
 });
 
 //change the what the nav looks like on scroll
 
-window.onscroll = function () {
-  const header = document.querySelector('header');
-  const navLinks = document.querySelectorAll('header nav a');
-  const menuTogglerdivs = document.querySelectorAll('#menuToggler div');
-  if (window.scrollY > 100 ){
-    for(const menuTogglerdiv of menuTogglerdivs){menuTogglerdiv.classList.add("menuBlack")}
-    for(const navLink of navLinks){navLink.classList.add("navLinkBlack")}
-    header.classList.add("headerWhite")
-  }else{
-    for(const menuTogglerdiv of menuTogglerdivs){menuTogglerdiv.classList.remove("menuBlack")}
-    for(const navLink of navLinks){navLink.classList.remove("navLinkBlack")}
-    header.classList.remove("headerWhite")
-  }
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const navLinks = document.querySelectorAll('header nav a');
+    const menuTogglerdivs = document.querySelectorAll('#menuToggler div');
+    if (window.scrollY > 100) {
+        for (const menuTogglerdiv of menuTogglerdivs) {
+            menuTogglerdiv.classList.add("menuBlack")
+        }
+        for (const navLink of navLinks) {
+            navLink.classList.add("navLinkBlack")
+        }
+        header.classList.add("headerWhite")
+    } else {
+        for (const menuTogglerdiv of menuTogglerdivs) {
+            menuTogglerdiv.classList.remove("menuBlack")
+        }
+        for (const navLink of navLinks) {
+            navLink.classList.remove("navLinkBlack")
+        }
+        header.classList.remove("headerWhite")
+    }
 };
 
 //start of API
-  async function loadObject(userID) {
-  const url = `https://corsanywhere.herokuapp.com/https://steamcommunity.com/inventory/${userID}/252490/2?l=english&count=5000`;
-  const response = await fetch(url);
-  return response.json();
+async function loadObject(userID) {
+    const url = `https://corsanywhere.herokuapp.com/https://steamcommunity.com/inventory/${userID}/252490/2?l=english&count=5000`;
+    const response = await fetch(url);
+    return response.json();
 }
 
 //Convert object data into DOM elements
 function buildArticleFromData(specificitem) {
-  //create the  elements
-  const article = document.createElement("article");
-  const h3 = document.createElement("h3");
-  const img = document.createElement("img");
-  const marketplacelink = document.createElement("a")
-  const workshoplink = document.createElement("a")
-  //acesssing the data
-  h3.innerText=(specificitem.name);
-  img.src=("https://steamcommunity-a.akamaihd.net/economy/image/"  + specificitem.icon_url_large);
-  marketplacelink.text = "Marketplace";
-  marketplacelink.href = "https://steamcommunity.com/market/listings/252490/"+ specificitem.name;
-  let specificworkshoplink= "";
-  try{
-    specificworkshoplink=specificitem.actions[0].link
-    workshoplink.text = "Workshop";
-    specificworkshoplink= "https://rustlabs.com/skin/" + (specificitem.name.replace(/ /g,"-").toLowerCase());
-  }
-  catch(err) {
-    workshoplink.text = "Facepunch Item";
-    specificworkshoplink= "https://rustlabs.com/skin/" + (specificitem.name.replace(/ /g,"-").toLowerCase());
-  }
-  workshoplink.href = specificworkshoplink;
-  workshoplink.target = "_blank";
-  marketplacelink.target = "_blank";
-  //add each item
-  article.appendChild(img);
-  article.appendChild(h3);
-  article.classList.add("youritem");
-  article.appendChild(marketplacelink);
-  article.appendChild(workshoplink);
-  return article;
+    //create the  elements
+    const article = document.createElement("article");
+    const h3 = document.createElement("h3");
+    const img = document.createElement("img");
+    const marketplacelink = document.createElement("a")
+    const workshoplink = document.createElement("a")
+    //acesssing the data
+    h3.innerText = (specificitem.name);
+    img.src = ("https://steamcommunity-a.akamaihd.net/economy/image/" + specificitem.icon_url_large);
+    marketplacelink.text = "Marketplace";
+    marketplacelink.href = "https://steamcommunity.com/market/listings/252490/" + specificitem.name;
+    let specificworkshoplink = "";
+    try {
+        specificworkshoplink = specificitem.actions[0].link
+        workshoplink.text = "Workshop";
+        specificworkshoplink = "https://rustlabs.com/skin/" + (specificitem.name.replace(/ /g, "-").toLowerCase());
+    } catch (err) {
+        workshoplink.text = "Facepunch Item";
+        specificworkshoplink = "https://rustlabs.com/skin/" + (specificitem.name.replace(/ /g, "-").toLowerCase());
+    }
+    workshoplink.href = specificworkshoplink;
+    workshoplink.target = "_blank";
+    marketplacelink.target = "_blank";
+    //add each item
+    article.appendChild(img);
+    article.appendChild(h3);
+    article.classList.add("youritem");
+    article.appendChild(marketplacelink);
+    article.appendChild(workshoplink);
+    return article;
 }
 
 //function to insert the articles into the pages
 
 async function insertArticles(search, userID) {
-  let attemptToSearch=false;
-  loader.classList.add("waiting");
-  // get list of items in inventory
-  const obj = await loadObject(userID);
-  //check for search
-  try{
-    yourItemsArray = obj.descriptions;
-    if(search != ""){
-      attemptToSearch=true;
-      //check to see if search matches any results
-      for (const a of yourItemsArray){
-        let name=a.market_name;
-        name=name.toLowerCase();
-        name=name.replace(/ /g,'')
-        if(name.includes(search)){
-          if(attemptToSearch==true){
-            attemptToSearch=false;
-            yourItemsArray = [];}
-          yourItemsArray.push(a)
-        }}}
-    //split the results into pages
-    let myObjects = yourItemsArray.slice((currentPage - 1) * pageSize, currentPage * pageSize);
-    if(attemptToSearch==true){
-      count.textContent = "No results found";
-      myObjects=[];}
-    else{count.textContent = "found " + yourItemsArray.length + " results for " + search;}
-    nPages.textContent = Math.ceil(yourItemsArray.length / pageSize);
-    // set the currentPage
-    for(const item of myObjects){
-      const article = buildArticleFromData(item);
-      yourItems.appendChild(article);
-      loader.classList.remove("hidden");
-    }}
-  //error handling if search failed or no inventory available for specified userID
-  catch{count.textContent = "No results found";}
+    let attemptToSearch = false;
+    loader.classList.add("waiting");
+    // get list of items in inventory
+    const obj = await loadObject(userID);
+    //check for search
+    try {
+        yourItemsArray = obj.descriptions;
+        if (search != "") {
+            attemptToSearch = true;
+            //check to see if search matches any results
+            for (const a of yourItemsArray) {
+                let name = a.market_name;
+                name = name.toLowerCase();
+                name = name.replace(/ /g, '')
+                if (name.includes(search)) {
+                    if (attemptToSearch == true) {
+                        attemptToSearch = false;
+                        yourItemsArray = [];
+                    }
+                    yourItemsArray.push(a)
+                }
+            }
+        }
+        //split the results into pages
+        let myObjects = yourItemsArray.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+        if (attemptToSearch == true) {
+            count.textContent = "No results found";
+            myObjects = [];
+        } else {
+            count.textContent = "found " + yourItemsArray.length + " results for " + search;
+        }
+        nPages.textContent = Math.ceil(yourItemsArray.length / pageSize);
+        // set the currentPage
+        for (const item of myObjects) {
+            const article = buildArticleFromData(item);
+            yourItems.appendChild(article);
+            loader.classList.remove("hidden");
+        }
+    }
+    //error handling if search failed or no inventory available for specified userID
+    catch {
+        count.textContent = "No results found";
+    }
 }
 
 //function to load the page
 
 async function loadPage(search, userID) {
-  pageIndicator.textContent = currentPage;
-  loader.classList.add("waiting");
-  loaderText.classList.remove("hidden");
-  clearyourItems();
-  await insertArticles(search, userID);
-  loader.classList.remove("waiting");
-  loaderText.classList.add("hidden");
+    pageIndicator.textContent = currentPage;
+    loader.classList.add("waiting");
+    loaderText.classList.remove("hidden");
+    clearyourItems();
+    await insertArticles(search, userID);
+    loader.classList.remove("waiting");
+    loaderText.classList.add("hidden");
 }
 
 //clear the current list
 function clearyourItems() {
-  while(yourItems.childNodes.length > 3) {
-    yourItems.lastChild.remove();
-  }
+    while (yourItems.childNodes.length > 3) {
+        yourItems.lastChild.remove();
+    }
 }
 
 //function to take you to next page
 
 function nextPage() {
-  let userID = playerSearch.value;
-  currentPage += 1;
-  const nPages = Math.ceil(yourItemsArray.length / pageSize);
-  if(currentPage > nPages) { currentPage = 1;}
-  loadPage(searchcriteria, userID);
+    let userID = playerSearch.value;
+    currentPage += 1;
+    const nPages = Math.ceil(yourItemsArray.length / pageSize);
+    if (currentPage > nPages) {
+        currentPage = 1;
+    }
+    loadPage(searchcriteria, userID);
 }
 
 //function to take you to previous page
 
 function prevPage() {
-  let userID = playerSearch.value;
-  currentPage -= 1;
-  const nPages = Math.ceil(yourItemsArray.length / pageSize);
-  if(currentPage < 1) { currentPage = nPages;}
-  loadPage(searchcriteria, userID);
+    let userID = playerSearch.value;
+    currentPage -= 1;
+    const nPages = Math.ceil(yourItemsArray.length / pageSize);
+    if (currentPage < 1) {
+        currentPage = nPages;
+    }
+    loadPage(searchcriteria, userID);
 }
 prev.addEventListener('click', prevPage);
 next.addEventListener('click', nextPage);
 
 //Search bar
 
-searchButton.addEventListener('click', ev =>{
-  let userID = playerSearch.value;
-  searchcriteria=itemSearch.value.toLowerCase();
-  searchcriteria=searchcriteria.replace(/ /g,'');
-  if(userID!= ""){
+searchButton.addEventListener('click', ev => {
+    let userID = playerSearch.value;
+    searchcriteria = itemSearch.value.toLowerCase();
+    searchcriteria = searchcriteria.replace(/ /g, '');
+    if (userID != "") {
 
-    loadPage(searchcriteria, userID);
-  }
-  currentPage = 1;
-  pageIndicator.innerText = "1";
+        loadPage(searchcriteria, userID);
+    }
+    currentPage = 1;
+    pageIndicator.innerText = "1";
 });
 
 //ID event listener
 
-playerSearchButton.addEventListener('click', ev =>{
-  let userID = playerSearch.value;
-  searchcriteria=itemSearch.value.toLowerCase();
-  searchcriteria=searchcriteria.replace(/ /g,'');
-  if(userID!= ""){
-    loadPage(searchcriteria, userID)
-    pageIndicator.innerText = "1";
-  }
-  currentPage = 1;
+playerSearchButton.addEventListener('click', ev => {
+    let userID = playerSearch.value;
+    searchcriteria = itemSearch.value.toLowerCase();
+    searchcriteria = searchcriteria.replace(/ /g, '');
+    if (userID != "") {
+        loadPage(searchcriteria, userID)
+        pageIndicator.innerText = "1";
+    }
+    currentPage = 1;
 });
